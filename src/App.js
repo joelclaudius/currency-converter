a08eb9a7e1257d0385d58e4893aefd1f53f74911
Claudius Joel <otiiclaudius@gmail.com>
2024-06-11 21:31:48 +0300

    Update build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
-function App() {
+import React from "react";
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const [currency, setCurrency] = useState("");
+  const [countryA, setCountryA] = useState("");
+  const [countryB, setCountryB] = useState("");
+  const [output, setOutput] = useState(null);
+
+  useEffect(() => {
+    const fetchCurrency = async () => {
+      if (currency && countryA && countryB) {
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${currency}&from=${countryA}&to=${countryB}`
+        );
+        const data = await res.json();
+
+        console.log(data);
+        // console.log(data.rates);
+        console.log(data.rates[countryB]);
+        setOutput(data.rates[countryB]);
+      }
+    };
+
+    fetchCurrency();
+  }, [currency, countryA, countryB]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter currency"
+          value={currency}
+          onChange={(e) => setCurrency(e.target.value)}
+        />
+        <select value={countryA} onChange={(e) => setCountryA(e.target.value)}>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="CAD">CAD</option>
+          <option value="INR">INR</option>
+        </select>
+        <select value={countryB} onChange={(e) => setCountryB(e.target.value)}>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="CAD">CAD</option>
+          <option value="INR">INR</option>
+        </select>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {currency && countryA && countryB ? (
+            <span> {output}</span>
+          ) : (
+            <span>OUTPUT</span>
+          )}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
